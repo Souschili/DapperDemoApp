2e9468e5df76ec2b0b3dc3725f479e3e20331332
Orkhan <aliyevorhan82@gmail.com>
2026-01-02 01:47:53 +0400

    add DbSession

--- DapperDemoApp/Db/DbSession.cs
+++ DapperDemoApp/Db/DbSession.cs
@@ -1,6 +1,39 @@
-﻿namespace DapperDemoApp.Db
+﻿using System.Data;
+
+namespace DapperDemoApp.Db
 {
-    public class DbSession
+    public sealed class DbSession : IDisposable
     {
+        public IDbConnection Connection { get; } = default!;
+        public IDbTransaction? Transaction { get;private set; }
+
+
+        public DbSession(IDbConnection connection)
+        {
+            Connection = connection;
+            Connection.Open();
+        }
+
+        public void BeginTransaction()
+        {
+            Transaction = Connection.BeginTransaction();
+        }
+
+        public void Commit()
+        {
+            Transaction?.Commit();
+        }
+
+        public void Rollback()
+        {
+            Transaction?.Rollback();
+        }
+
+
+        public void Dispose()
+        {
+            Connection?.Dispose();
+            Transaction?.Dispose();
+        }
     }
 }
